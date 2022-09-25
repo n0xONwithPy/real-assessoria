4f9605af7727e6acd559b5a388749a1e05a0a55e
unknown <johnny.trucolo97@gmail.com>
2022-09-25 15:59:12 -0300

    Ajustes no codigo CSS

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -1,4 +1,4 @@
 ScrollReveal().reveal('#primary-text');
-ScrollReveal().reveal('#primary-text', { origin: 'left' });
 ScrollReveal().reveal('#primary-text', { delay: 500 });
 ScrollReveal().reveal('#primary-text', { duration: 2000 });
+ScrollReveal().reveal('#primary-text', { origin: 'left' });
