cfa996b199d026c1b518c22dc2f2aed1e872a691
unknown <johnny.trucolo97@gmail.com>
2022-10-16 11:28:29 -0300

    Code review CSS n Script

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -1,8 +1,8 @@
-ScrollReveal().reveal('.title', { delay: 600 });
-ScrollReveal().reveal('.title', { duration: 2000 });
+ScrollReveal().reveal('.title', { delay: 400 });
+ScrollReveal().reveal('.title', { duration: 1000 });
 
-ScrollReveal().reveal('.desc', { delay: 600 });
-ScrollReveal().reveal('.desc', { duration: 2000 });
+ScrollReveal().reveal('.desc', { delay: 400 });
+ScrollReveal().reveal('.desc', { duration: 1000 });
 
-ScrollReveal().reveal('#midias', {delay: 600});
-ScrollReveal().reveal('#midias', { duration: 2000 });
+ScrollReveal().reveal('#midias', {delay: 400});
+ScrollReveal().reveal('#midias', { duration: 1000 });
