a5ff4bf7884321169a87bdc2b24eb9f3fc809753
n0xOnWithPy <johnny.trucolo97@gmail.com>
2022-10-19 11:17:57 -0300

    Darkmode implement on demand

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,13 +1,4 @@
 function darkMode() {
-    let elemento = document.body;
-    let conteudo = document.getElementById("darkModeText");
-    elemento.className = "dark-mode";
-    conteudo.innerText = "Dark Mode ON";
-}
-
-function lightMode() {
-    let elemento = document.body;
-    let conteudo = document.getElementById("darkModeText");
-    elemento.className = "light-mode";
-    conteudo.innerText = "Light Mode ON";
-}
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
