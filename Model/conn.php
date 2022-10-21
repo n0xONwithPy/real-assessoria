1c01246cb7092d379aeb889cd8f1ee1f9b8d0a5b
n0xON <92261947+n0xONwithPy@users.noreply.github.com>
2022-10-21 10:46:47 -0300

    Update CONNECTION

--- Model/conn.php
+++ Model/conn.php
@@ -1,17 +1,18 @@
 <?php
-
-//CONEXAO AO BANCO DE DADOS PARA COLETA DE INFORMAÇOES VIA POST DO FORMULARIO
-//REAL ASSESSORIA EMPRESARIAL
+//CONEXÃO COM BASE DE DADOS ONDE FICARAM ARMAZENADOS OS DADOS DO FORMULARIO DE CONTATO
 
 define ("HOST","localhost");
 define ("USER","root");
 define ("DB","");
 define ("PW","");
 
+$msg = FALSE;
+
 try {
    $conn = mysqli_connect(HOST,USER,PW);
     mysqli_select_db($conn,DB);
 }
 catch (Exception $e) {
-    //PLANEJANDO SOBRE OS POSSIVEIS ERROS DE CONEXAO
+   $msg = "Erro na conexão";
+   echo $msg.$e->getMessage();
 }
